1bd7480ccee0fa133afe5abc622d257cef57f16e
Sahib <sahibzulfigar4@gmail.com>
2022-06-17 15:26:36 +0200

    Revert "Revert "Debug""
    This reverts commit 0a68b29e9fd7e8b6160e9ef38a73c126964ad0d2.

--- Wondereiz/components/PartecipantBox.js
+++ Wondereiz/components/PartecipantBox.js
@@ -1,56 +1,55 @@
 import React from 'react'
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 
-const PartecipantBox = ({post}) => {
-    return (
-        <View style={{ marginBottom: 30 }}>
-            <PartecipantBoxHeader post={post}/>
-        </View>
-    )
+const PartecipantBox = ({ post }) => {
+  return (
+    <View style={{ marginBottom: 30 }}>
+      <PartecipantBoxHeader post={post} />
+    </View>
+  )
 }
 
 const PartecipantBoxHeader = ({ post }) => (
-    <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Image source={{ uri: post.partecipant_picture }} style={styles.partecipantBox} />
-          <Text
-            style={{
-              color: "#4B4B4B",
-              fontWeight: "500",
-              fontSize: 18,
-              marginLeft: 35,
-              marginTop: 10
-            }}
-          >
-            {post.caption}
-          </Text>
-      </View>
-      <TouchableOpacity>
-            <Image
-              source={{
-                uri: "https://img.icons8.com/ios-glyphs/30/D50FBC/back.png",
-              }}
-              style={styles.icon}
-            />
-          </TouchableOpacity>
+  <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <Image source={{ uri: post.partecipant_picture }} style={styles.partecipantBox} />
+      <Text style={styles.caption}>
+        {post.caption}
+      </Text>
     </View>
+    <TouchableOpacity>
+      <Image
+        source={{
+          uri: "https://img.icons8.com/ios-glyphs/30/D50FBC/back.png",
+        }}
+        style={styles.icon}
+      />
+    </TouchableOpacity>
+  </View>
 )
 
 const styles = StyleSheet.create({
-    partecipantBox: {
-        width: 50,
-        height: 50,
-        marginLeft: 10,
-        marginTop: 15,
-        borderRadius: 40,
-    },
-    icon: {
-      height: 30,
-      width: 30,
-      marginTop: 15,
-      marginRight: 20,
-      transform: [{ scaleX: -1 }]
-    },
+  partecipantBox: {
+    width: 50,
+    height: 50,
+    marginLeft: 10,
+    marginTop: 15,
+    borderRadius: 40,
+  },
+  icon: {
+    height: 30,
+    width: 30,
+    marginTop: 15,
+    marginRight: 20,
+    transform: [{ scaleX: -1 }]
+  },
+  caption: {
+    color: "#4B4B4B",
+    fontWeight: "500",
+    fontSize: 18,
+    marginLeft: 35,
+    marginTop: 10
+  }
 })
 
 export default PartecipantBox
