b49acaa44cbc4a74512919bed087a8a16dc1cb50
Sahib <sahibzulfigar4@gmail.com>
2022-06-22 16:13:22 +0200

    Notifications page is done

--- Wondereiz/screens/NotificationsScreen.js
+++ Wondereiz/screens/NotificationsScreen.js
@@ -18,79 +18,116 @@ import {
   doc,
   getDoc,
   limit,
+  getDocs,
 } from "firebase/firestore";
 import { ScrollView } from "react-native-gesture-handler";
 import Loading from "../components/Loading";
+import {
+  useCollection,
+  useCollectionOnce,
+} from "react-firebase-hooks/firestore";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function NotificationsPage(props) {
   const userUid = app.auth().currentUser.uid;
-
-  let [notifications, setNcotifications] = useState([]);
   let [result, setResult] = useState([]);
-
   let q;
 
-  const getRoomIds = () => {};
-
   useEffect(() => {
-    let roomIds = ["room6"];
     let rooms = [];
     let messages = [];
     let results = [];
 
-    const myUser = doc(db, "User", userUid);
+    q = query(
+      collection(db, "Room"),
+      where("membersUserId", "array-contains", userUid)
+    );
 
-    getDoc(myUser).then((snapshot) => {
-      roomIds = snapshot.data().newMessages;
-
-      Promise.all(roomIds).then((roomIds) => {
-        let q = query(collection(db, "Room"), where("id", "in", roomIds));
-        onSnapshot(q, (snapshot) => {
-          if (snapshot) {
-            snapshot.docs.forEach((room) => {
-              rooms.push(room.data());
-            });
-            Promise.all(rooms).then((rooms) => {
-              rooms.forEach((room) => {
-                q = query(
-                  collection(db, "Messages"),
-                  where("roomId", "==", room.id)
-                );
-                onSnapshot(q, (snapshot) => {
-                  if (snapshot) {
-                    messages.push({
-                      ...room,
-                      text: snapshot.docs[0].data().text,
-                      senderUid: snapshot.docs[0].data().uid,
+    getDocs(q).then((snapshot) => {
+      snapshot.docs.forEach((e) => {
+        rooms.push(e.data());
+      });
+      Promise.all(rooms).then(() => {
+        rooms.forEach((room, i) => {
+          q = query(collection(db, "Messages"), where("roomId", "==", room.id));
+          onSnapshot(q, (snapshot) => {
+            if (snapshot) {
+              messages.push({
+                ...room,
+                text: snapshot.docs[snapshot.docs.length - 1].data().text,
+                senderUid: snapshot.docs[snapshot.docs.length - 1].data().uid,
+              });
+              Promise.all(messages).then(() => {
+                const sender = doc(db, "User", messages[i].senderUid);
+                getDoc(sender).then((snapshot) => {
+                  if (snapshot.exists) {
+                    console.log(messages[i].text);
+                    results.push({
+                      ...messages[i],
+                      sendersName: snapshot.data().firstName,
                     });
-                    Promise.all(messages).then((messages) => {
-                      const sender = doc(
-                        db,
-                        "User",
-                        messages[messages.length - 1].senderUid
-                      );
-                      getDoc(sender).then((snapshot) => {
-                        if (snapshot.exists) {
-                          results.push({
-                            ...messages[messages.length - 1],
-                            sendersName: snapshot.data().firstName,
-                          });
-                          Promise.all(results).then((results) => {
-                            setResult(results);
-                            console.log(result);
-                          });
-                        }
-                      });
+                    Promise.all(results).then((results) => {
+                      setResult(results);
                     });
                   }
                 });
               });
-            });
-          }
+            }
+          });
         });
       });
     });
   }, []);
+
+  let [rooms, setRooms] = useState([]);
+  let [messages, setMessages] = useState([]);
+
+  // useEffect(() => {
+  //   let snapshotRooms = [];
+  //   q = query(
+  //     collection(db, "Room"),
+  //     where("membersUserId", "array-contains", userUid)
+  //   );
+  //   getDocs(q).then((snapshot) => {
+  //     if (snapshot) {
+  //       snapshot.docs.map((e) => {
+  //         snapshotRooms.push(e.data());
+  //       });
+  //       setRooms(snapshotRooms);
+  //     }
+  //   });
+  // }, []);
+
+  // useEffect(() => {
+  //   let snapshotMessages = [];
+  //   if (rooms.length == 0) {
+  //     return;
+  //   }
+  //   rooms.forEach((e) => {
+  //     console.log(e.id);
+  //     q = query(collection(db, "Message"), where("roomId", "==", e.id));
+  //     getDoc(q).then((querySnapshot) => {
+  //       //console.log(querySnapshot.data());
+  //     });
+  //   });
+  //   //setMessages(snapshotMessages);
+  // }, [rooms]);
+
+  // useEffect(() => {
+  //   if (messages.length == 0) {
+  //     return;
+  //   }
+  //   messages.forEach((e, i) => {
+  //     console.log(e);
+  //     // q = query(collection(db, "Message"), where("roomId", "==", e.id));
+
+  //     // getDoc(q).then((querySnapshot) => {
+  //     //   rooms[i] += querySnapshot.data();
+  //     // });
+  //   });
+  // }, [messages]);
+
+  //console.log(messages.docs.length);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -101,7 +138,9 @@ export default function NotificationsPage(props) {
           result.map((data, idx) => (
             <TouchableOpacity
               style={styles.messageBox}
-              onPress={() => props.navigation.navigate("ChatScreen", {room: result[idx]})} 
+              onPress={() =>
+                props.navigation.navigate("ChatScreen", { room: result[idx] })
+              }
             >
               <Image
                 style={styles.messageImg}
